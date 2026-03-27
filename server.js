fb1f8b5533e7827f0f4b71e1020e1a576aeaab89
thurahtun999 <thuratunfamily1810@gmail.com>
2026-03-27 23:04:16 +0630

    fix server.js

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.post("/api/stripe/webhook", express.raw ({ type: "*/*"}),
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use("uploads", express.static("uploads"))
 
 app.use("/api/stripe",stripeRoutes);
 app.use ("/api/users", userRoutes);
