eaa86235851f073d53c7aeb0f291b173558e421e
thurahtun999 <thuratunfamily1810@gmail.com>
2026-03-27 20:53:25 +0630

    fix server.js

--- server.js
+++ server.js
@@ -108,9 +108,6 @@ mongoose.connect(process.env.MONGO_URI). then(() => {
     console.log("MongoDB connected");
 });
 
-app.use(notfound);
-app.use(errorHandler);
-
 app.get("/", (req, res) => {
     res.send("E-commerce API Running...");
 });
@@ -120,5 +117,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+
+app.use(notfound);
+app.use(errorHandler);
+
 
  
