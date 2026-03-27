ed395a0a011f680927354eb352d42754f3855d73
thurahtun999 <thuratunfamily1810@gmail.com>
2026-03-27 21:21:11 +0630

    fix port for railway

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get("/", (req, res) => {
     res.send("E-commerce API Running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
