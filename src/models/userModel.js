424707eee8b576c4f609d726af30af7961f9ca72
thurahtun999 <thuratunfamily1810@gmail.com>
2026-03-28 13:14:56 +0630

    fix userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, "Password is required"],
+        unique: true,
     },
     role: {
         type: String,
