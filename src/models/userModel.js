ed1d0f948b75710b67858bd134f15b5f7c87c109
thurahtun999 <thuratunfamily1810@gmail.com>
2026-03-28 13:06:16 +0630

    fix userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,9 +1,22 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+
+    },
+
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
     role: {
         type: String,
         enum: ["user", "admin"],
